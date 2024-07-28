f33b2680c3bd282206af04d384137bb6c01aa854
Bang HoJin <132210541+banhogu@users.noreply.github.com>
2024-07-28 20:59:28 +0900

    [Silver II] Title: 유기농 배추, Time: 188 ms, Memory: 12260 KB -BaekjoonHub

---
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.js
@@ -0,0 +1,45 @@
+const input = require("fs")
+  .readFileSync("dev/stdin")
+  .toString()
+  .trim()
+  .split("\n");
+
+let testCases = Number(input[0]);
+let line = 1;
+
+while (testCases--) {
+  let [m, n, k] = input[line].split(" ").map(Number); //10 가로,  8 세로, 17 배추갯수
+  let graph = [];
+  for (let i = 0; i < n; i++) {
+    graph[i] = new Array(m);
+  }
+  for (let i = 1; i <= k; i++) {
+    let [y, x] = input[line + i].split(" ").map(Number);
+    graph[x][y] = 1;
+  }
+  let answer = 0;
+  for (let i = 0; i < n; i++) {
+    for (let j = 0; j < m; j++) {
+      if (dfs(graph, n, m, i, j)) {
+        answer++;
+      }
+    }
+  }
+  line += k + 1;
+  console.log(answer);
+}
+
+function dfs(graph, n, m, x, y) {
+  if (x <= -1 || x >= n || y <= -1 || y >= m) {
+    return false;
+  }
+  if (graph[x][y] == 1) {
+    graph[x][y] = -1;
+    dfs(graph, n, m, x - 1, y);
+    dfs(graph, n, m, x, y - 1);
+    dfs(graph, n, m, x + 1, y);
+    dfs(graph, n, m, x, y + 1);
+    return true;
+  }
+  return false;
+}
