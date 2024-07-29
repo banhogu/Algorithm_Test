d99a69fb2c668259816a28060d7fcb66da8d5fd1
Bang HoJin <132210541+banhogu@users.noreply.github.com>
2024-07-29 16:27:19 +0900

    [level 2] Title: 주식가격, Time: 35.28 ms, Memory: 41.8 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.js
+++ 프로그래머스/2/42584. 주식가격/주식가격.js
@@ -1,15 +1,18 @@
 function solution(prices) {
-    let answer = [];
+    var answer = [];
     
-    for(let i=0; i<prices.length; i++ ){
+    
+    for(let i=0; i<prices.length; i++){
         let count = 0
-        for(let j=i+1; j<prices.length; j++ ){
-            count += 1
-            if(prices[i] > prices[j]){
-                break
-            }
+        for(let j=i+1; j<prices.length; j++){
+            count++
+            if(prices[i] > prices[j])
+               break
         }
         answer.push(count)
     }
-    return answer;
+    return(answer)
+    
+    
+    // return answer;
 }
