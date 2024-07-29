a8735e844ca6988b6bfdd395c15fc5af9b956981
Bang HoJin <132210541+banhogu@users.noreply.github.com>
2024-07-29 15:28:22 +0900

    [level 2] Title: 올바른 괄호, Time: 2.89 ms, Memory: 37 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.js
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.js
@@ -1,10 +1,17 @@
 function solution(s){
-    let count = 0
-    for(let i=0; i<s.length; i++){
-        s[i] === "(" ? count+= 1 : count-=1
-        if(count < 0 ){
-            break
+    let count = 0;
+
+    for(let i = 0; i < s.length; i++){
+        if(s[i] === "("){
+            count++;
+        }
+        else if(s[i] === ")"){
+            count--;
+            if(count < 0){
+                return false;
+            }
         }
     }
-    return count === 0 ? true : false
+    
+    return count === 0;
 }
