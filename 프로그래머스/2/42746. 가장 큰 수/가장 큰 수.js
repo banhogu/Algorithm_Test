62688378f307ccf2b8b8391ac57f3a2ff21a3196
Bang HoJin <132210541+banhogu@users.noreply.github.com>
2024-07-29 23:43:25 +0900

    [level 2] Title: 가장 큰 수, Time: 176.44 ms, Memory: 44.8 MB -BaekjoonHub

---
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.js
@@ -0,0 +1,11 @@
+function solution(numbers) {
+    let answer = numbers.map((item)=>String(item)).sort((a,b)=>(
+    (b+a) - (a+b) 
+    )).join("")
+    
+    return answer[0] === '0' ? '0' : answer
+    
+    
+        
+    
+}
