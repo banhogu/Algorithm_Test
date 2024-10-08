513b9b6c5694ef11e13f73a1f9e9a1c9dafff68f
Bang HoJin <132210541+banhogu@users.noreply.github.com>
2024-10-08 19:13:19 +0900

    [level 2] Title: 전화번호 목록, Time: 124.31 ms, Memory: 78.9 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.js
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.js
@@ -2,9 +2,9 @@ function solution(phone_book) {
     const arr = phone_book.sort()
     
     for(let i=0; i<arr.length-1; i++){
-        if(arr[i] == arr[i+1].slice(0,arr[i].length)){
+        if(arr[i] === arr[i+1].slice(0,arr[i].length)){
             return false
         }
     }
-    return true;
+    return true
 }
