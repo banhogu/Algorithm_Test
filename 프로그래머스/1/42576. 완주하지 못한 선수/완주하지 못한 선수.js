1f7d7657f4764298be43522a73d1a7330f7a17a7
Bang HoJin <132210541+banhogu@users.noreply.github.com>
2024-10-08 19:04:53 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 62.82 ms, Memory: 62.2 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
@@ -1,25 +1,25 @@
 function solution(participant, completion) {
-    let answer = '';
-    let bag = new Map()
-    participant.forEach((item)=>{
-        if(bag.has(item)){
-            bag.set(item,bag.get(item)+1)
-        }
-        else{
-            bag.set(item,1)
-        }
-    })
+    let answer = ''
+   let bag = new Map()
+   participant.forEach((item)=>{
+       if(bag.has(item)){
+           bag.set(item, bag.get(item)+1)
+       }
+       else{
+           bag.set(item,1)
+       }
+   })
     completion.forEach((item)=>{
-        if(bag.get(item)===1){
-            bag.delete(item)
-        }
-        else{
-            bag.set(item,bag.get(item)-1)
+        if(bag.has(item)){
+            bag.set(item, bag.get(item)-1)
         }
     })
-    for(const[key,value] of bag){
-        answer = key
+    
+    for([key,value] of bag){
+        if(value === 1){
+            answer = key
+        }
     }
     
-    return answer;
+    return(answer)
 }
